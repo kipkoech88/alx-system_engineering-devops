2bd0a0968bf6135912d41e9da96ba8da0bc23803
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-15 13:40:58 -0700

    fixed , error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,6 +3,6 @@ exec { 'command':
      command => 'apt -y update;
      apt-get -y install nginx;
      sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-     service nginx restart;'
-     provider => shell
+     service nginx restart;',
+     provider => shell,
      }
