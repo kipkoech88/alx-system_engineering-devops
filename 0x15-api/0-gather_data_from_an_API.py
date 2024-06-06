9fed5f251842dd884cf64bfdb44edfa2c9a15370
kipkoech88 <hoseakipkoech88@gmail.com>
2024-06-05 22:19:16 -0700

    ensure it is int

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,11 +8,15 @@ import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    param = sys.argv[1]
-    user = requests.get(url + "users/{}".format(param)).json()
-    todos = requests.get(url + "todos", params={"UserId": param}).json()
+    param = int(sys.argv[1])
+    if type(param) is not int:
+        print("error fetching APi")
+    else:
+        user = requests.get(url + "users/{}".format(param)).json()
+        todos = requests.get(url + "todos", params={"UserId": param}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        completed = [t.get("title") for t in todos if t.get("completed")
+                     is True]
+        print("Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), len(completed), len(todos)))
+        [print("\t {}".format(c)) for c in completed]
